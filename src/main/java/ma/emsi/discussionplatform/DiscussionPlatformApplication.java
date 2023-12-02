2fee84a262d44cd535c22d99d2cd211996359ff2
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-12-03 00:32:17 +0100

    first commit

---
+++ src/main/java/ma/emsi/discussionplatform/DiscussionPlatformApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.discussionplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionPlatformApplication.class, args);
+	}
+
+}
